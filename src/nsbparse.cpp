e2451e38afd79282f5b719503dd8556635f2de0b
Mislav Blažević <krofnica996@gmail.com>
2013-10-13 18:37:26 +0200

    nsbparse works :)

--- src/nsbparse.cpp
+++ src/nsbparse.cpp
@@ -1,14 +1,33 @@
 #include "nsbfile.hpp"
 
 #include <iostream>
-
+#include <fstream>
+#include <cstring>
 int main(int argc, char** argv)
 {
-    if (argc != 2)
+    if (argc != 3)
     {
-        std::cout << "usage: " << argv[0] << " <file.nsb>" << std::endl;
+        std::cout << "usage: " << argv[0] << " <input.nsb> <output.nss>" << std::endl;
         return 1;
     }
 
-    NsbFile(argv[1], NSB_COMPILED);
+    NsbFile Script(argv[1], NSB_COMPILED);
+    std::ofstream File(argv[2]);
+    while (Line* pLine = Script.GetNextLine())
+    {
+        std::cout << "Writing magic: " << std::hex << SWAP_UINT16(pLine->Magic) << std::endl;
+        uint32_t i = 0;
+
+        // Cause pLine->Magic == MAGIC_CALL is bugged...
+        if (strcmp("CALL", NsbFile::StringifyMagic(pLine->Magic)) == 0)
+            File << pLine->Params[i++];
+        else
+            File << NsbFile::StringifyMagic(pLine->Magic);
+
+        File << "(";
+        for (; i < pLine->Params.size(); ++i)
+            File << pLine->Params[i] << ((i != (pLine->Params.size() - 1)) ? ", " : "");
+        File << ");\n";
+        File << std::flush;
+    }
 }
