6a4286f32ffb25be87dfcd7c555d4f43b5869d32
Mislav Blažević <krofnica996@gmail.com>
2013-10-13 13:15:46 +0200

    Initial nsbparse

--- src/nsbparse.cpp
+++ src/nsbparse.cpp
@@ -1,5 +1,14 @@
 #include "nsbfile.hpp"
 
+#include <iostream>
+
 int main(int argc, char** argv)
 {
+    if (argc != 2)
+    {
+        std::cout << "usage: " << argv[0] << " <file.nsb>" << std::endl;
+        return 1;
+    }
+
+    NsbFile(argv[1], NSB_COMPILED);
 }
