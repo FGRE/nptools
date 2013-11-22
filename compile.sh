23f261f470459fa0b08abd52508a0b523548936a
Mislav Blažević <krofnica996@gmail.com>
2013-11-22 11:21:41 +0100

    Replace compile.sh with CMakeLists.txt

--- compile.sh
+++
@@ -1,4 +0,0 @@
-g++ -o npaextract -Wl,-rpath ../libnpa -ggdb -lboost_locale -lboost_filesystem -lboost_system --std=c++11 -I ../libnpa/include -lnpa -L ../libnpa/ src/npaextract.cpp
-g++ -o npapack -Wl,-rpath ../libnpa -ggdb --std=c++11 -I ../libnpa/include -lnpa -lboost_locale -lboost_filesystem -lboost_system -L ../libnpa/ src/npapack.cpp
-g++ -o nsbparse -Wl,-rpath ../libnpa -ggdb --std=c++11 -I ../libnpa/include -lnpa -lboost_locale -L ../libnpa/ src/nsbparse.cpp
-g++ -o nsbcompile -Wl,-rpath ../libnpa -ggdb --std=c++11 -I ../libnpa/include -lnpa -lboost_locale -L ../libnpa/ src/nsbcompile.cpp
