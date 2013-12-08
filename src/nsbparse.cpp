91c23498c0110ac7793a4b9c52af571754ad7819
Mislav Blažević <krofnica996@gmail.com>
2013-12-08 11:01:57 +0100

    Remove UNK magic collector and add option to print line numbers

--- src/nsbparse.cpp
+++ src/nsbparse.cpp
@@ -28,41 +28,50 @@ int main(int argc, char** argv)
 {
     if (argc < 2 || argc > 3)
     {
-        std::cout << "usage: " << argv[0] << " <input.nsb> [output.nss]" << std::endl;
+        std::cout << "usage: " << argv[0] << "[-l] <input.nsb>" << std::endl;
         return 1;
     }
 
-    std::string Output;
+    bool PrintLineNumbers = false;
     if (argc == 2)
     {
-        Output = argv[1];
-        Output[Output.size() - 1] = 's';
-        Output = Output.substr(Output.find_last_of("/") + 1);
+        if (argv[1][0] == '-')
+        {
+            std::cout << "No input file specified" << std::endl;
+            return 1;
+        }
     }
     else
-        Output = argv[2];
+    {
+        if (std::string(argv[1]) != "-l")
+        {
+            std::cout << "Invalid option specified" << std::endl;
+            return 1;
+        }
+        else
+            PrintLineNumbers = true;
+    }
 
-    std::vector<uint16_t> UnkMagic;
-    NsbFile Script(argv[1]);
+    std::string Output = argv[PrintLineNumbers ? 2 : 1];
+    Output[Output.size() - 1] = 's';
+    Output = Output.substr(Output.find_last_of("/") + 1);
+
+    NsbFile Script(argv[PrintLineNumbers ? 2 : 1]);
     std::ofstream File(Output);
-    uint16_t unk = MAGIC_UNKMAX;
-
     while (Line* pLine = Script.GetNextLine())
     {
         uint32_t i = 0;
 
+        if (PrintLineNumbers)
+            File << Script.GetNextLineEntry() - 1 << " ";
+
         if (!NsbFile::IsValidMagic(pLine->Magic))
         {
-            if (std::find(UnkMagic.begin(), UnkMagic.end(), pLine->Magic) == UnkMagic.end())
-            {
-                std::cout << std::uppercase << "MAGIC_UNK" << std::dec << unk++ << " = " << "SWAP_UINT16(0x"
-                          << std::hex << uint16_t(SWAP_UINT16(pLine->Magic)) << ")," << std::endl;
-                UnkMagic.push_back(pLine->Magic);
-            }
+            std::cout << "Unknown magic: " << std::hex << pLine->Magic << std::dec << std::endl;
             continue;
         }
 
-        if (pLine->Magic == uint16_t(MAGIC_CALL) || pLine->Magic == uint16_t(MAGIC_FUNCTION_BEGIN))
+        if (pLine->Magic == MAGIC_CALL || pLine->Magic == MAGIC_FUNCTION_BEGIN)
             File << pLine->Params[i++];
         else
             File << NsbFile::StringifyMagic(pLine->Magic);
@@ -73,13 +82,9 @@ int main(int argc, char** argv)
         File << ");\n";
     }
 
-    for (uint32_t i = 0; i < UnkMagic.size(); ++i)
-        std::cout << "(" << "MAGIC_UNK" << std::dec << unk - UnkMagic.size() + i << ", "
-                  << "\"UNK" << unk - UnkMagic.size() + i << "\")" << std::endl;
-
     // Open Map file
-    std::strcpy(&argv[1][std::strlen(argv[1]) - 3], "map");
-    MapFile MFile(argv[1]);
+    std::strcpy(&argv[PrintLineNumbers ? 2 : 1][std::strlen(argv[PrintLineNumbers ? 2 : 1]) - 3], "map");
+    MapFile MFile(argv[PrintLineNumbers ? 2 : 1]);
     std::strcpy(&Output[Output.size() - 3], "map");
     std::ofstream MapOutput(Output);
 
