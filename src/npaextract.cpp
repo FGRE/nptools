9cf81c9ab2bad0789c633f1d0215a83a8a60c2e5
Mislav Blažević <krofnica996@gmail.com>
2014-07-04 20:53:06 +0200

    Port npaextract to new libnpa (add support for all games)

--- src/npaextract.cpp
+++ src/npaextract.cpp
@@ -15,29 +15,82 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * */
-#include "inpafile.hpp"
+#include "inipafile.hpp"
+#include "isgfile.hpp"
 #include "fscommon.hpp"
 #include <iostream>
 
+const char* NipaGames[] =
+{
+    "ChaosHead",
+    "ChaosHeadTR1",
+    "ChaosHeadTR2",
+    "MuramasaTR",
+    "Muramasa",
+    "Sumaga",
+    "Django",
+    "DjangoTR",
+    "Lamento",
+    "LamentoTR",
+    "SweetPool",
+    "SumagaSP",
+    "Demonbane",
+    "MuramasaAD",
+    "Axanel",
+    "Kikokugai",
+    "SoniComiTR2",
+    "Sumaga3P",
+    "SoniComi",
+    "LostX",
+    "LostXTrailer",
+    "DramaticalMurder",
+    nullptr
+};
+
+uint8_t GameStringToID(std::string String)
+{
+    for (int i = 0; NipaGames[i]; ++i)
+        if (String == NipaGames[i])
+            return i;
+    return -1;
+}
+
+int PrintArguments(char** argv)
+{
+    std::cout << "Supported games:\nSteinsGate\n";
+    for (int i = 0; NipaGames[i]; ++i)
+        std::cout << NipaGames[i] << '\n';
+    std::cout << "usage: " << argv[0] << " <file.npa> <game> [charset]" << std::endl;
+    return 1;
+}
+
 int main(int argc, char** argv)
 {
-    if (argc < 2 || argc > 3)
-    {
-        std::cout << "usage: " << argv[0] << " <file.npa> [charset]" << std::endl;
-        return 1;
-    }
+    if (argc < 3 || argc > 4)
+        return PrintArguments(argv);
 
-    if (argc == 3)
-        NpaFile::SetLocale(argv[2]);
+    if (argc == 4)
+        NpaFile::SetLocale(argv[3]);
     else
         NpaFile::SetLocale("ja_JP.CP932");
 
-    INpaFile Archive(argv[1]);
-    for (INpaFile::NpaIterator iter = Archive.Begin(); iter != Archive.End(); ++iter)
+    INpaFile* pArchive;
+    if (std::strcmp(argv[2], "SteinsGate") == 0)
+        pArchive = new ISGFile(argv[1]);
+    else
     {
-        char* pData = Archive.ReadFile(iter);
+        uint8_t GameID = GameStringToID(argv[2]);
+        if (GameID == -1)
+            return PrintArguments(argv);
+        pArchive = new INipaFile(argv[1], GameID);
+    }
+
+    for (INipaFile::NpaIterator iter = pArchive->Begin(); iter != pArchive->End(); ++iter)
+    {
+        // TODO: if IsNotDirectory
+        char* pData = pArchive->ReadFile(iter);
         std::cout << "Writing file: " << iter->first << std::endl;
-        fs::WriteFileDirectory(iter->first, pData, iter->second.Size);
+        fs::WriteFileDirectory(iter->first, pData, pArchive->GetFileSize(iter));
         delete[] pData;
     }
 }
