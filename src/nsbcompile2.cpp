770dbc2bc847f7f7bb3b5a5ee3f8b6c3bbd6b74c
Mislav Blažević <krofnica996@gmail.com>
2016-05-17 23:21:01 +0200

    nptools 0.5.1

--- src/nsbcompile2.cpp
+++ src/nsbcompile2.cpp
@@ -1,6 +1,6 @@
 /* 
  * nsbcompile2: Nitroplus script compiler
- * Copyright (C) 2014-2015 Mislav Blažević <krofnica996@gmail.com>
+ * Copyright (C) 2014-2016 Mislav Blažević <krofnica996@gmail.com>
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
