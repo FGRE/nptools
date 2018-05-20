391b652959f91705146eb25225ef581f13294e0f
Mislav Blažević <mislav.blazevic5@gmail.com>
2018-05-20 22:21:17 +0200

    nsbparse: write labels too

--- src/nsbparse.cpp
+++ src/nsbparse.cpp
@@ -1,17 +1,17 @@
-/* 
+/*
  * nsbparse: .nsb script decompiler
  * Copyright (C) 2013-2016,2018 Mislav Blažević <krofnica996@gmail.com>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * */
@@ -40,6 +40,10 @@ int main(int argc, char** argv)
     Output = Output.substr(Output.find_last_of("/") + 1);
 
     ScriptFile Script(argv[1], ScriptFile::NSB);
+    map<uint32_t, vector<string> > Symbols;
+    for (auto& i : Script.GetSymbols())
+        Symbols[i.second].push_back(i.first);
+
     ofstream File(Output);
     int indent = -1;
     uint32_t SourceIter = 0;
@@ -65,6 +69,13 @@ int main(int argc, char** argv)
         File << "(";
         for (uint32_t i = 0; i < pLine->Params.size(); ++i)
             File << pLine->Params[i] << ((i != (pLine->Params.size() - 1)) ? ", " : "");
-        File << ");\n";
+        File << ");";
+
+        auto i = Symbols.find(SourceIter - 1);
+        if (i != Symbols.end())
+            for (string& s : i->second)
+                File << ' ' << s;
+
+        File << '\n';
     }
 }
